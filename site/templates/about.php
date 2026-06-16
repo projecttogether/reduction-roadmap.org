15257bdd0b9943a28161069beebdb2ed99b56fbf
joh-sch <johannes@schmoll.studio>
2026-06-16 14:32:22 +0200

    misc

--- site/templates/about.php
+++ site/templates/about.php
@@ -1,7 +1,7 @@
 <?php snippet('header') ?>
 
 <main id="page">
-  <section class="py-16 bg-off-white">
+  <section class="pt-16 bg-off-white">
     <div class="container">
       <h1 class="font-heading font-black text-[clamp(2rem,5vw,3.5rem)] leading-[1.1] m-0"><?= $page->title()->html() ?></h1>
     </div>
