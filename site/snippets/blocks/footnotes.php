6f905cada6e6c988b8722b14e4b4a98c0b485a55
joh-sch <johannes@schmoll.studio>
2026-09-13 11:44:29 +0200

    misc

--- site/snippets/blocks/footnotes.php
+++ site/snippets/blocks/footnotes.php
@@ -10,10 +10,10 @@ if ($notes->isEmpty()) return;
 ///// Markup /////
 ////////////////// ?>
 
-<ol class="list-none m-0 p-0 text-sm space-y-1">
+<ol class="list-none m-0 p-0 text-sm space-y-2.5">
   <?php foreach ($notes as $note): ?>
     <li id="footnote-<?= esc($note->number()) ?>" class="flex gap-6 leading-snug">
-      <span class="shrink-0 font-medium"><?= $note->number()->html() ?>.</span>
+      <span class="shrink-0 text-fließtext-xs"><?= $note->number()->html() ?>.</span>
       <span><?= $note->text()->kt() ?></span>
     </li>
   <?php endforeach ?>
